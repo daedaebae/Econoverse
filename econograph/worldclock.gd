f36edcc54c7357697d4460e3fc219d12b675d490
daedae <david.s.murphy2@gmail.com>
2025-08-21 20:30:22 -0400

    Godot

---
+++ econograph/worldclock.gd
@@ -0,0 +1,23 @@
+extends Node
+@onready var world_clock: Label = $WorldClock
+
+var counter = 0
+var eventtimer = Timer.new()
+var time = str(Time.get_time_string_from_system())
+
+# Event time to trigger events
+func _on_timer_timeout():
+	eventtimer.stop()
+	eventtimer.timeout
+	print("Oh shit what now?!?")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	eventtimer.start()
+	counter += 1
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	world_clock.text = str(Time.get_ticks_msec()/1000)
+	pass
