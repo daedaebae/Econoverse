baa573600662165c5f41959d532d60a3e1fed0a6
daedaebae <92129349+daedaebae@users.noreply.github.com>
2025-09-01 20:59:06 -0400

    From my phone
    feat: created character.gd Node2d class

---
+++ Econoverse/scripts/character.gd
@@ -0,0 +1,26 @@
+class_name Character extends Node2d
+
+# Character attributes (e.g., gender, role, race)
+@export var gender: Array[String] = ["Male", "Female", "Other"]
+@export var profession: Array[String] = ["Culinary", "Miller", "Mason", "Smith", "Jewler", "Carpenter", "Tanner", "Brewer", "Stable-Master"]
+@export var race: Array[String] = ["Human", "Dwarf", "Gnome", "Elf", "Orc", "Dragonborn"]
+
+# Exported variables for the character's stats
+@export var influence: int = 0
+@export var fluency: int = 0
+@export var diplomacy: int = 0
+@export var negotiation: int = 0
+#@export var disposition: int = 0
+
+# Optional: Add a method to reset the character's stats
+func reset():
+    influence = 0
+    fluency = 5
+    diplomacy = 0
+    negotiation = 0
+    #disposition = 5
+
+# TODO: Fix methods to calculate derived values
+func get_char_attr(attr) -> int:
+    attr = 0
+    return attr
