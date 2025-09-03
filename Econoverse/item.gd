9e68400e8ce5dc144228423425f430fcb9081b5d
daedae <david.s.murphy2@gmail.com>
2025-09-02 22:29:03 -0400

    fix: Moved back to godot_mono, fixed accidental overwrite of curpish Balloon.tscn dialogue commits and .cs dependencies. feat: added to item.gd in prep for generating commodities feat: drafted detect_click.gd function for clicking and such style: added commidties dict, commented out while we figure out classes style: commented out Inventory.gd in prep to figure out classes

--- Econoverse/item.gd
+++ Econoverse/item.gd
@@ -1,6 +1,8 @@
 @tool
 extends Resource
 class_name Item
+# TODO: durf 09/02/25 - Can we rename this "commodities" and leave the option for "items" open
+#		for other things?
 
 @export var id : int
 @export var start_price : int
@@ -8,9 +10,13 @@ class_name Item
 @export var name_abbreviated : String = "abrv" #useful for cramped interfaces, extensive lists
 @export_multiline var description : String = ""
 @export var texture: Texture2D
+@export var world_supply: int = 1000
 
 # Not necessary to use. Could be a simple way to establish rules for quests etc. 
 # Doesn't need to be a player-forward value, let the world/characters emphasize the value.
+
+# durf 09/02/25 - I like this a lot, but will it be determined by the
+# world_supply or world_supply + distribution between merchants (supply/demand)?
 enum Rarity { COMMON , UNCOMMON , RARE }
 @export var rarity : Rarity = Rarity.COMMON
 
@@ -23,3 +29,48 @@ func get_tooltip() -> String:
 	tooltip_text += "Value: " + str(start_price) + "\n"
 	# tooltip_text += "Rarity: " + str(rarity)
 	return tooltip_text
+
+func get_id() -> int:
+	return id
+
+func set_start_price(new_start_price: int):
+	start_price = new_start_price
+
+func get_start_price() -> int:
+	return start_price
+
+func set_name_full(new_name_full: String):
+	name_full = new_name_full
+
+func get_name_full() -> String:
+	return name_full
+
+func set_name_abbreviated(new_name_abbreviated: String):
+	name_abbreviated = new_name_abbreviated
+
+func get_name_abbreviated() -> String:
+	return name_abbreviated
+
+func set_description(new_description: String):
+	description = new_description
+
+func get_description() -> String:
+	return description
+
+func set_texture(new_texture: Texture2D):
+	texture = new_texture
+
+func get_texture() -> Texture2D:
+	return texture
+
+func set_world_supply(new_world_supply: int):
+	world_supply = new_world_supply
+
+func get_world_supply() -> int:
+	return world_supply
+
+func set_rarity(new_rarity: Rarity):
+	rarity = new_rarity
+
+func get_rarity() -> Rarity:
+	return rarity
---
+++ Econoverse/scripts/detect_click.gd
@@ -0,0 +1,26 @@
+# durf 09/02/25 - prep for mouseover and click on commodities and items
+
+
+# extends Node
+
+# # When slot is pressed
+# func _on_texture_button_gui_input(event):
+# 	if event is InputEventMouseButton and event.pressed:
+# 		if event.button_index == MOUSE_BUTTON_LEFT:
+# 			slot_input.emit(
+# 				self, InventorySlotAction.SELECT
+# 			)
+# 		elif event.button_index == MOUSE_BUTTON_RIGHT:
+# 			slot_input.emit(
+# 				self, InventorySlotAction.SPLIT
+# 			)
+
+
+
+# func _on_texture_button_mouse_entered():
+# 	slot_hovered.emit(self, true)
+
+
+
+# func _on_texture_button_mouse_exited():
+# 	slot_hovered.emit(self, false)
