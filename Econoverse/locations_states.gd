8822cfe36595e75bb161bd6d0d0ce3b22e793c3e
daedae <david.s.murphy2@gmail.com>
2025-09-11 23:28:11 -0400

    draftfeat: created locations scene, locations nodes, and instantiation draft

---
+++ Econoverse/locations_states.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# TODO: Need to update these to Godot 4 syntax
+#@onready var locations = {
+	#BaseState.locations.Lumber_Mill: $Lumber_Mill,
+	#BaseState.locations.Smithy: $Smithy,
+	#BaseState.locations.Stables: $Stables,
+	#BaseState.locations.Bakery: $Bakery,
+	#BaseState.locations.Brewhouse: $Brewhouse,
+	#BaseState.locations.Masonic_Shop: $Masonic_Shop,
+	#BaseState.locations.Tannery: $Tannery,
+	#BaseState.locations.Town_Square: $Town_Square
+#}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
