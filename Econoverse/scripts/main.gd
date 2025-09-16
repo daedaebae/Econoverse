8c306a961069fa6509bf26482d8a5adcbdd1c9ba
daedae <david.s.murphy2@gmail.com>
2025-09-16 01:07:54 -0400

    Trades!
    feat: hardcoded inventory items into dicts of char objects triforce.
    
    feat: created trade function in char class to give/get amounts of items to/from whom!
    
    feat: added curp's dialogue with conditionals referencing objects
    
    feat: bought strudel for 5 coins!

--- Econoverse/scripts/main.gd
+++ Econoverse/scripts/main.gd
@@ -10,9 +10,10 @@ var CharScene = load("res://characters.tscn")
 var CharSceneInstance = CharScene.instantiate()
 var Char = CharSceneInstance
 
-var Player = Character.new("Iona",7,1,"Human",0,{"Coins": 10})
-var Smith = Character.new("Bron",1,0,"Human",0,{"Coins": 700})
-var Baker = Character.new("Tiebyrn",3,"Male","Human",4,{"Coins": 400})
+#durf 09/15/25 - Hardcoding inventory and items for v1.0
+var Player = Character.new("Iona",7,1,"Human",0,{"Sword": 0, "Strudel": 0, "Coins": 5})
+var Smith = Character.new("Bron",1,0,"Human",0,{"Sword": 1, "Strudel": 0, "Coins": 700})
+var Baker = Character.new("Tiebyrn",3,"Male","Human",4,{"Sword": 0, "Strudel": 1, "Coins": 0})
 
 #@export var Stablemaster = Character.new(
 	#"Brygna",
@@ -81,7 +82,18 @@ func test_shit(Test, CharIn):
 		"\nlocation: ",CharIn.location,
 		"\ngender: ",CharIn.gender,
 		"\nrace: ",CharIn.race,
-		"\ncoin: ",CharIn.inventory.Coins,
+		"\ninventory:\n\t\t[Sword: ",CharIn.inventory.Sword,
+		"] [Strudel: ",CharIn.inventory.Strudel,"] [Coins: ",CharIn.inventory.Coins,
+		"] ",
 		"\nprofession: ",CharIn.profession
 	)
+	#print(
+		#"#######\nTrade prep\n#######"
+	#)
+	#if CharIn == Player:
+		#Player.trade(Baker, "Coins", 5, "Strudel", 1)
+	#print(
+			#"Seems they did the trade!\nPlayer Inv: ",Player.inventory,
+			#"\nBaker Inv:",Baker.inventory
+	#)
 	pass
