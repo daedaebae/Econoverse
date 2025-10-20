fffecc117fc95923db15be83590643c290cbd1ca
durf <david.s.murphy2@gmail.com>
2025-10-20 18:55:29 -0400

    Character and inventory scenes and framework

--- Econoverse_v2/assets/scripts/main.gd
+++ Econoverse_v2/assets/scripts/main.gd
@@ -6,15 +6,17 @@ extends Node
 
 #region Create Character scene and objects
 
+var player = load("res://scenes/player.tscn")
+var playerScene = player.instantiate()
 var CharScene = load("res://characters.tscn")
 var CharSceneInstance = CharScene.instantiate()
 var Char = CharSceneInstance
 
 #durf 09/15/25 - Hardcoding inventory and items for v1.0
-var Player = Character.new("Iona",7,1,"Human",0,{"Sword": 0, "Strudel": 0, "Coins": 5}, false)
-var Smith = Character.new("Bron",1,0,"Human",0,{"Sword": 1, "Strudel": 0, "Coins": 700}, false)
-var Baker = Character.new("Tiebyrn",3,"Female","Human",4,{"Sword": 0, "Strudel": 1, "Coins": 0}, false)
-var Guard = Character.new("Roric Harthorne",7,0,"Human",0,{"Sword": 0, "Strudel":0, "Coins": 200}, false)
+var Player = player.new("Iona",7,1,"Human",0,{"Sword": 0, "Strudel": 0, "Coins": 5}, false)
+#var Smith = Character.new("Bron",1,0,"Human",0,{"Sword": 1, "Strudel": 0, "Coins": 700}, false)
+#var Baker = Character.new("Tiebyrn",3,"Female","Human",4,{"Sword": 0, "Strudel": 1, "Coins": 0}, false)
+#var Guard = Character.new("Roric Harthorne",7,0,"Human",0,{"Sword": 0, "Strudel":0, "Coins": 200}, false)
 
 #@export var Stablemaster = Character.new(
 	#"Brygna",
