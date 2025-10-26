8659c77ee4ad01d55ab42c749d84dcd80a843b2e
curpish <ravenoftheredsky@gmail.com>
2025-10-26 18:18:35 -0400

    script notes, implied pseudocode

--- Econoverse_v2/singleton/audio_master_script.gd
+++ Econoverse_v2/singleton/audio_master_script.gd
@@ -2,8 +2,11 @@ extends Node
 
 #kc 10/26/25;
 
+#connect to any signals. consider a full-on signal bus? 
+
 #load all sounds
 
 #sfx levels, user defined values
 
-#methods for playing sounds
+#methods for playing sounds, based on signals.
+#variety of sound modulation to give a dynamic feel.
