574dd3c0fc1164eef3f27b47f631e63ae21d541c
curpish <ravenoftheredsky@gmail.com>
2025-10-26 18:16:04 -0400

    feat - audio master, sfx implemented
    audio master scene and script will manage any signals and respond by playing sound based on criteria. The current implementation is simplistic tree-based identifiers for sounds. The script will be designed to listen for any events and play sounds based on the passed values etc. This will build off of our current design and allow for even more extensible and reactive, emergent gameplay and feel.

--- Econoverse_v2/singleton/ui_trade.gd
+++ Econoverse_v2/singleton/ui_trade.gd
@@ -11,13 +11,21 @@ extends Control
 @export var get_item_label: Label
 @export var get_amount_label: Label
 
-##TODO: not yet implemented##
-# Make globals for these to use in other ui?
-@export var sound_trade: AudioStreamPlayer2D
-@export var sound_cancel: AudioStreamPlayer2D
-@export var sound_plus: AudioStreamPlayer2D
-@export var sound_minus: AudioStreamPlayer2D
-##
+# --- Sound ---
+##kc 10/26/25; began basic sfx implementation. Not the most  
+## rigid yet. Also considering variations based on
+## signal values and extensible game logic.
+@onready var sound_show: AudioStreamPlayer = $SfxPapyrusOpen
+@onready var sound_hide: AudioStreamPlayer = $SfxPapyrusClose
+
+
+@onready var sound_plus: AudioStreamPlayer = $SfxIncrement
+@onready var sound_minus: AudioStreamPlayer = $SfxDecrement
+
+## 10/26/25; these need to specifically react to 
+## trade criteria and signals for successful or failed trades.
+#@export var sound_trade_complete: AudioStreamPlayer
+#@export var sound_trade_fail: AudioStreamPlayer
 
 # --- Trade State ---
 # These variables will store the current trade details
@@ -53,7 +61,7 @@ func open_trade(player: Node, npc: Node, item_give: String, item_get: String):
 	# Update labels and show the window
 	_update_labels()
 	show()
-
+	sound_show.play()
 
 # A helper function to keep all our labels in sync
 func _update_labels():
@@ -78,8 +86,12 @@ func _on_button_trade_pressed() -> void:
 	if player_node and player_node.has_method("trade"):
 		player_node.trade(npc_node, current_give_amount, current_item_give, current_get_amount, current_item_get)
 		# Close the UI after the trade
-		#TODO: play sound for complete
+		#TODO: kc 10/26/25; for now, this will simply play the 
+		# close window sound. Later, can play specific sounds
+		# for if trade was successful or declined... or crit? idk
+		sound_hide.play()
 		hide()
+
 	else:
 		print("Error: Player node not set or 'trade' function missing.")
 
@@ -87,28 +99,30 @@ func _on_button_trade_pressed() -> void:
 func _on_button_minus_get_pressed() -> void:
 	current_get_amount = max(0, current_get_amount - 1)
 	_update_labels()
-	#TODO: play sound for minus
+	sound_minus.play()
 
 
 func _on_button_plus_get_pressed() -> void:
 	current_get_amount += 1
 	# Add logic here to check if NPC has enough
 	_update_labels()
-	#TODO: play sound for plus
+	sound_plus.play()
 
 
 func _on_button_cancel_pressed() -> void:
 	hide()
+	sound_hide.play()
 
 
 func _on_button_minus_give_pressed() -> void:
 	current_give_amount = max(0, current_give_amount - 1)
 	_update_labels()
-	#TODO: play sound for minus
+	sound_minus.play()
+
 
 
 func _on_button_plus_give_pressed() -> void:
 	current_give_amount += 1
 	# Add logic here to check if player has enough
 	_update_labels()
-	#TODO: play sound for plus
+	sound_plus.play()
