f17296ce5669aaf1f167703f9c27044ac7257982
durf <92129349+daedaebae@users.noreply.github.com>
2026-03-12 18:47:00 -0400

    Update Resource_LedgerResource.gd
    Added current price

--- Econoverse_v2/scripts/commodities/Resource_LedgerResource.gd
+++ Econoverse_v2/scripts/commodities/Resource_LedgerResource.gd
@@ -8,17 +8,19 @@ class_name LedgerResource
 
 @export var id = 0
 @export var start_price = 0
+@export var current_price = 0
 @export var name_full = ""
 @export var name_abbreviated = ""
 @export var description = ""
 #TODO: set image variable
-#@export var texture 
+#@export var texture = ("res://assets/...")
 @export var world_supply = 1000
 @export var rarity = Item.Rarity.COMMON
 
 @export var ledger = {
 		"id": id,
 		"start_price": start_price,
+		"current_price": current_price,
 		"name_full": name_full,
 		"name_abbreviated": name_abbreviated,
 		"description": description,
